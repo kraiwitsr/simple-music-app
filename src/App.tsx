e7c4b8acf2c4ddcf1e1291628225e3799a093e22
kraiwit samanpiew <kraiwit.chokunsr@gmail.com>
2023-06-25 18:46:30 +0700

    Add random music feature when page is start

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import Playing from './components/playing';
 import { Config } from './resource/config';
 
 const App: React.FC = () => {
+
+  const RandomSongs = (): number => {
+    const length = Config.length;
+    const RandomIndex = Math.floor(Math.random() * length);
+    return RandomIndex;
+  }
+
+  const SongsNumber: number = RandomSongs();
   const [songs, setSongs] = React.useState(Config);
   const [isPlaying, setisPlaying] = React.useState(false);
-  const [currentSong, setCurrentSong] = React.useState(Config[2]);
+  const [currentSong, setCurrentSong] = React.useState(Config[SongsNumber]);
   const audioElement = React.useRef<HTMLAudioElement>(null);
 
   React.useEffect(() => {
